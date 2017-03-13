bb39bbd8eaf8450a1e4ee15362ab21437b2f5076
timchair1 <thetimothygan@gmail.com>
2017-03-12 22:43:52 -0500

    maybe implemented gen file reading

--- GladiusGreenfoot/Gladius/MyWorld.java
+++ GladiusGreenfoot/Gladius/MyWorld.java
@@ -1,6 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.*;
 import java.awt.*;
+import java.io.*;
 /**
  * Write a description of class MyWorld here.
  * 
@@ -15,13 +16,14 @@ public class MyWorld extends World
      VisionCone jacobVision;
      Gladius james;
      Gladius jacob;
+     Scanner scanner;
      double x;
      int gah = 0;
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
+    public MyWorld() throws Exception
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1300, 700, 1); 
@@ -29,6 +31,7 @@ public class MyWorld extends World
         getBackground().fill();
         Arena arena = new Arena();
         addObject(arena,650,350);
+        scanner = new Scanner(new File("FWeights.txt"));
         /*
         for(int i = 1; i< 3; i++)
         {
@@ -155,7 +158,7 @@ public class MyWorld extends World
              jacob.inputCone(0);
          }
          gah++;
-         if(gah == 100)
+         if(gah == 100 && scanner.hasNextLine())
          {
              gah = 0;
             int jamesx = Greenfoot.getRandomNumber(999)+151;
@@ -165,7 +168,81 @@ public class MyWorld extends World
             int jacx = Greenfoot.getRandomNumber(999)+151;
             int jacy = Greenfoot.getRandomNumber(499)+101;
             jacob.setRotation(Greenfoot.getRandomNumber(365));
-             
+            
+            
+            scanner.nextLine();
+           String[][] theta1 = new String[3][4];
+           theta1[0] = scanner.nextLine().split(" ");
+           theta1[1] = scanner.nextLine().split(" ");
+           theta1[2] = scanner.nextLine().split(" ");
+           scanner.nextLine();
+           String[][] theta2 = new String[4][4];
+           theta2[0] = scanner.nextLine().split(" ");
+           theta2[1] = scanner.nextLine().split(" ");
+           theta2[2] = scanner.nextLine().split(" ");
+           theta2[3] = scanner.nextLine().split(" ");
+           scanner.nextLine();
+           String[][] theta3 = new String[4][4];
+           theta3[0] = scanner.nextLine().split(" ");
+           theta3[1] = scanner.nextLine().split(" ");
+           theta3[2] = scanner.nextLine().split(" ");
+           theta3[3] = scanner.nextLine().split(" ");
+           double[][] t1 = new double[3][4];
+           double[][] t2 = new double[4][4];
+           double[][] t3 = new double[4][4];
+           double[][] t11 = new double[3][4];
+           double[][] t22 = new double[4][4];
+           double[][] t33 = new double[4][4];
+           scanner.nextLine();
+           String[][] theta11 = new String[3][4];
+           theta11[0] = scanner.nextLine().split(" ");
+           theta11[1] = scanner.nextLine().split(" ");
+           theta11[2] = scanner.nextLine().split(" ");
+           scanner.nextLine();
+           String[][] theta22 = new String[4][4];
+           theta22[0] = scanner.nextLine().split(" ");
+           theta22[1] = scanner.nextLine().split(" ");
+           theta22[2] = scanner.nextLine().split(" ");
+           theta22[3] = scanner.nextLine().split(" ");
+           scanner.nextLine();
+           String[][] theta33 = new String[4][4];
+           theta33[0] = scanner.nextLine().split(" ");
+           theta33[1] = scanner.nextLine().split(" ");
+           theta33[2] = scanner.nextLine().split(" ");
+           theta33[3] = scanner.nextLine().split(" ");
+           scanner.nextLine();
+           for(int r = 0; r < 3; r++)
+           {
+               for(int c = 0; c < 4; c++)
+               {
+                   t1[r][c] = Double.parseDouble(theta1[r][c]);
+                   t11[r][c] = Double.parseDouble(theta11[r][c]);
+               }
+           }
+           for(int r = 0; r < 4; r++)
+           {
+               for(int c = 0; c < 4; c++)
+               {
+                   t2[r][c] = Double.parseDouble(theta2[r][c]);
+                   t22[r][c] = Double.parseDouble(theta22[r][c]);
+                   t3[r][c] = Double.parseDouble(theta3[r][c]);
+                   t33[r][c] = Double.parseDouble(theta33[r][c]);
+               }
+           }
+         }
+         else if(gah == 100)
+         {
+             gah = 0;
+            int jamesx = Greenfoot.getRandomNumber(999)+151;
+            int jamesy = Greenfoot.getRandomNumber(499)+101;
+            james.setRotation(Greenfoot.getRandomNumber(365));
+            james.setLocation(jamesx,jamesy);
+            int jacx = Greenfoot.getRandomNumber(999)+151;
+            int jacy = Greenfoot.getRandomNumber(499)+101;
+            jacob.setRotation(Greenfoot.getRandomNumber(365));
+         }
+         else
+         {
          }
          //jacob.act();
          //jacobVision.act();
